be6c5a09031c187e6725a1d1d3df16cba5778317
Bach <bachtom125@gmail.com>
2023-05-16 16:38:10 +0700

    Home Screen with Mock Data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,9 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import productScreen from "./screen/productScreen.jsx";
+// import productScreen from "./screen/ProductScreen.jsx";
 import "./bootstrap.min.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/product/:id" component={productScreen} />
+          <Route path="/" component={HomeScreen} exact />
+          {/* <Route path="/product/:id" component={productScreen} /> */}
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/sample-data/productsData.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: "ASUS ROG Falchion NX",
+    image: "/images/asus.jpg",
+    description:
+      "COMPACT, WITHOUT COMPROMISE - ROG Falchion NX is a 65% form-factor wireless mechanical keyboard designed to change the way you play. Interactive touch panel and innovative two-way cover case, plus it’s the first ROG keyboard with wireless Aura Sync RGB lighting.",
+    brand: "ASUS",
+    category: "Electronics",
+    price: 2000000,
+    countInStock: 15,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Samsung Galaxy Watch Active2",
+    image: "/images/galaxywatchactive2.jpg",
+    description:
+      "Stylish and functional smartwatch that tracks your fitness and health records. With GPS and LTE connectivity, you can take calls and messages on-the-go. AMOLED display provides vivid and clear viewing experience.",
+    brand: "Samsung",
+    category: "Electronics",
+    price: 199.99,
+    countInStock: 5,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Bose QuietComfort 35 II",
+    image: "/images/bose.jpg",
+    description:
+      "Noise-cancelling headphones that deliver balanced audio quality. With voice assistant integration, you can control your music and manage your tasks with ease. Bluetooth connectivity allows you to listen to your favorite tracks wirelessly.",
+    brand: "Bose",
+    category: "Electronics",
+    price: 299.99,
+    countInStock: 2,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Canon EOS R5",
+    image: "/images/canon.jpg",
+    description:
+      "Full-frame mirrorless camera that delivers high-quality images and videos. With a 45-megapixel sensor and 8K video capabilities, you can capture stunning visuals. The camera features a weather-sealed body for durability.",
+    brand: "Canon",
+    category: "Electronics",
+    price: 3799.99,
+    countInStock: 1,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Apple AirPods Pro",
+    image: "/images/airpod.jpg",
+    description:
+      "True wireless earbuds that feature active noise cancellation and spatial audio technology. With the H1 chip, you can easily connect to your Apple devices and switch between them. The earbuds come with multiple tips for a comfortable and secure fit.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 249.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Logitech G Pro Wireless",
+    image: "/images/gpro.jpg",
+    description:
+      "Wireless gaming mouse that provides reliable performance and accurate tracking. With a lightweight and ergonomic design, you can play your favorite games for hours without discomfort. RGB lighting adds a touch of personalization.",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 149.99,
+    countInStock: 8,
+    rating: 0,
+    numReviews: 0,
+  },
+
+  {
+    name: "Sony WH-1000XM4",
+    image: "/images/asus.jpg",
+    description:
+      "Noise-cancelling headphones that deliver high-quality audio and voice control. With up to 30 hours of battery life, you can listen to your favorite tracks without interruption. The headphones come with a carrying case for easy storage and transportation.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 349.99,
+    countInStock: 4,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Canon EOS R5",
+    image: "/images/canon.jpg",
+    description:
+      "Full-frame mirrorless camera that delivers high-quality images and videos. With a 45-megapixel sensor and 8K video capabilities, you can capture stunning visuals. The camera features a weather-sealed body for durability.",
+    brand: "Canon",
+    category: "Electronics",
+    price: 3799.99,
+    countInStock: 1,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+export default products;
